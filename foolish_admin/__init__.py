1ecdf9c84e31c3e559c3e3c9b015ce8493ff0f5c
Aditya Mukati <adityamukati8564@gmail.com>
2024-01-13 16:57:58 +0530

    modified gen_flag

--- foolish_admin/__init__.py
+++ foolish_admin/__init__.py
@@ -1,8 +1,8 @@
 from foolish_admin.main import app
-from foolish_admin import gen_flag
+#from foolish_admin import gen_flag
 
-file=open("challenge.txt","a+")
-if gen_flag.checker(file):
-    raise Exception("No challenge.txt file found")
+#file=open("challenge.txt","a+")
+#if gen_flag.checker(file):
+#    raise Exception("No challenge.txt file found")
 
-file.close()
+#file.close()
--- foolish_admin/gen_flag.py
+++ foolish_admin/gen_flag.py
@@ -11,15 +11,16 @@ def checker(file) -> bool:
     return file.read() == ""
 
 def challenge_checker():
-    file=open("challenge.txt","a+")
-    if len(sys.argv) == 1 and checker(file):
+    file=open("challenge.txt","r+")
+    if len(sys.argv) == 1:
         flag_input=input("Enter the flag text (Enter none for random characters string): ")
         flag = generator() if flag_input=="" else flag_input
+        file.seek(0)
         file.write(flag)
-    elif len(sys.argv) >= 2  and checker(file):
+    elif len(sys.argv) >= 2:
         file.write(sys.argv[1])
-    elif not checker(file):
-        print("challenge.txt file already exists with a valid flag")
+#    elif not checker(file):
+#        print("challenge.txt file already exists with a valid flag")
     file.close()
 
 if __name__=="__main__":
